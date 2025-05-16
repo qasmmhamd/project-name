7d8c7a35c11023522bbb66b6dbd9582fa3934bde
qasmmhamd <qasmabwrh37@gmail.com>
2025-05-16 14:23:15 +0300

    Refactor delete unused files

--- app/Http/Controllers/carController.php
+++ app/Http/Controllers/carController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Car;
 use Illuminate\Http\Request;
 
-class CarController extends Controller
+class CarController 
 {
     public function index()
     {
